774a6cbc7dd8e0599760a26727dc3c8bb85795c7
Willi Gierke <willi.gierke@student.hpi.uni-potsdam.de>
2016-10-27 23:19:30 +0200

    added initial tests and Circle config

---
+++ README.md
@@ -0,0 +1,1 @@
+# git_better [![CircleCI](https://circleci.com/gh/WGierke/git_better.svg?style=svg&circle-token=3fb4bac4bb656bc2e7b9dbb6d9dc77a303bd240c)](https://circleci.com/gh/WGierke/git_better)
---
+++ tests/test_integration.py
@@ -0,0 +1,13 @@
+import unittest2
+
+
+class TestUM(unittest2.TestCase):
+
+    def setUp(self):
+        pass
+
+    def test_numbers_3_4(self):
+        self.assertEqual(True, True)
+
+if __name__ == '__main__':
+    unittest2.main()
