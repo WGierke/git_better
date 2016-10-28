54125367e75c345e5c03c80cae5d7b60fc80f9eb
Willi Gierke <willi.gierke@futurice.com>
2016-10-28 15:06:15 +0200

    fixed closed_issues and isOwnerHomepage

--- feature_aggregation.py
+++ feature_aggregation.py
@@ -1,9 +1,5 @@
 """
 Aggregate the provided Data Frame with features we receive from the Github REST and GraphQL API
-GraphQL:    closed_issues, open_issues, closed_pull_requests, merged_pull_requests, open_pull_requests, projects, watchers,
-            stargazers, forks, mentionableUsers, description
-REST:       size, language_rate, readme
-Custom:     isOwnerHomepage, hasHomepage
 TODO:       commits_count
 """
 from tqdm import tqdm
@@ -22,10 +18,13 @@ def aggregate_features(data_frame):
         data_frame = add_graph_features(data_frame, index, owner, name)
         data_frame = add_rest_features(data_frame, index, repo)
         data_frame = add_custom_features(data_frame, index, owner, name)
+        data_frame = fix_closed_issues(data_frame, index)
     return data_frame
 
 
 def add_graph_features(data_frame, index, owner, name):
+    """closed_issues, open_issues, closed_pull_requests, merged_pull_requests, open_pull_requests,
+    projects, watchers, stargazers, forks, mentionableUsers, description"""
     features = get_graph_features(owner, name)
     for k in features.keys():
         data_frame.set_value(index, k, features[k])
@@ -41,28 +40,45 @@ def get_graph_features(repo_owner, repo_name):
         if isinstance(data[k], dict):
             features[k] = data[k]['totalCount']
         else:
-            features[k] = data[k]
+            if data[k]:
+                features[k] = data[k]
+            else:
+                features[k] = ''
     return features
 
 
 def add_rest_features(data_frame, index, repo):
+    """size, language_rate, readme"""
     data_frame.set_value(index, 'size', repo.size)
-    # Readme
     try:
-        regex = re.compile('[^a-zA-Z0-9 :\/]')
+        regex = re.compile('[^a-zA-Z0-9 :\/-]')
         readme = regex.sub('', repo.get_readme().decoded_content)
         data_frame.set_value(index, 'readme', readme)
     except:
         data_frame.set_value(index, 'readme', '')
-    # Language_Rate
     sum_loc = sum(repo.get_languages().values())
     languages = repo.get_languages()
     for key in languages.keys():
-        data_frame.set_value(index, "LANGUAGE_" + key, languages[key] / float(sum_loc))
+        data_frame.set_value(index, "LANGUAGE_" + key,
+                             languages[key] / float(sum_loc))
     return data_frame
 
 
 def add_custom_features(data_frame, index, owner, name):
-    data_frame.set_value(index, "isOwnerHomepage", name == "{}.github.io".format(owner))
-    data_frame.set_value(index, "hasHomepage", website_exists("http://{}.github.io/{}".format(owner, name)))
+    """isOwnerHomepage, hasHomepage"""
+    data_frame.set_value(index, "isOwnerHomepage", name.lower() ==
+                         "{}.github.io".format(owner.lower()))
+    data_frame.set_value(index, "hasHomepage", website_exists(
+        "http://{}.github.io/{}".format(owner, name)))
+    return data_frame
+
+
+def fix_closed_issues(data_frame, index):
+    """ The sum of merged and closed pull requests must be subtracted from the
+    number of closed issues"""
+    old_closed_issues_count = data_frame.loc[index, "closed_issues"]
+    closed_pull_requests = data_frame.loc[index, "closed_pull_requests"]
+    merged_pull_requests = data_frame.loc[index, "merged_pull_requests"]
+    data_frame.set_value(index, "closed_issues", old_closed_issues_count -
+                         (closed_pull_requests + merged_pull_requests))
     return data_frame
