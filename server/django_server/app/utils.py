4a3abd0e308c9c4eb7459074290e70c3298a303a
Willi Gierke <willi.gierke@student.hpi.uni-potsdam.de>
2016-12-07 23:54:11 +0100

    added readme classification to view

--- server/django_server/app/utils.py
+++ server/django_server/app/utils.py
@@ -1,5 +1,7 @@
+import re
 from github import Github
-from app.training import load_pickle, JOBLIB_DESCRIPTION_PIPELINE_NAME
+from app.training import load_pickle, JOBLIB_DESCRIPTION_PIPELINE_NAME, JOBLIB_README_PIPELINE_NAME
+
 
 def add_repo_header(repo_list):
     return '''  <div class="repositories">
@@ -10,20 +12,17 @@ def add_repo_header(repo_list):
                                     <strong>Your repositories</strong>
                                     <span class="badge pull-right">{}</span>
                                 </div>
-                                <div class="col-md-4">
+                                <div class="col-md-3">
                                     <strong>Description</strong>
                                 </div>
-                                <div class="col-md-1">
+                                <div class="col-md-2">
                                     <strong>Class (Description)</strong>
                                 </div>
                                 <div class="col-md-2">
+                                    <strong>Class (Readme)</strong>
+                                </div>
+                                <div class="col-md-2">
                                     <strong>Language</strong>
-                                </div>
-                                <div class="col-md-1">
-                                    <strong>Stars</strong>
-                                </div>
-                                <div class="col-md-1">
-                                    <strong>Forks</strong>
                                 </div>
                             </div>
                         </li>
@@ -32,18 +31,22 @@ def add_repo_header(repo_list):
                 </div>'''.format(len(repo_list), '\n'.join(repo_list))
 
 
-def repo_list_html(repo_infos, model):
+def repo_list_html(repo_infos, descr_model, readme_model):
     repos_html = []
     for i in range(len(repo_infos)):
         name = repo_infos[i][0]
         descr = repo_infos[i][1]
-        language = repo_infos[i][2]
-        stars = repo_infos[i][3]
-        forks = repo_infos[i][4]
-        classified = model.predict([descr])[0]
+        readme = repo_infos[i][2]
+        language = repo_infos[i][3]
+        stars = repo_infos[i][4]
+        forks = repo_infos[i][5]
         classes = ['DATA', 'DEV', 'DOCS', 'EDU', 'HW', 'WEB']
-        classified_probas = dict(zip(classes, model.predict_proba([descr])[0]))
-        classified_proba = classified_probas[classified]
+        predicted_descr = descr_model.predict([descr])[0]
+        predicted_readme = readme_model.predict([readme])[0]
+        predicted_descr_probas = dict(zip(classes, descr_model.predict_proba([descr])[0]))
+        predicted_descr_proba = predicted_descr_probas[predicted_descr]
+        predicted_readme_probas = dict(zip(classes, readme_model.predict_proba([readme])[0]))
+        predicted_readme_proba = predicted_readme_probas[predicted_readme]
 
         repos_html.append(
             ''' <li class="list-group-item">
@@ -51,44 +54,47 @@ def repo_list_html(repo_infos, model):
                         <div class="col-md-3">
                             <a href="https://github.com/{name}">{name}</a>
                         </div>
-                        <div class="col-md-4">
+                        <div class="col-md-3">
                             {description}
                         </div>
-                        <div class="col-md-1">
-                            {classified} ({classified_proba:0.4f})
+                        <div class="col-md-2">
+                            {pred_descr} ({pred_descr_proba:0.4f})
+                        </div>
+                        <div class="col-md-2">
+                            {pred_readme} ({pred_readme_proba:0.4f})
                         </div>
                         <div class="col-md-2">
                             {language}
                         </div>
-                        <div class="col-md-1">
-                            <span class="star-badge pull-left">
-                                {stars}
-                                <span class="octicon octicon-star"></span>
-                            </span>
-                        </div>
-                        <div class="col-md-1">
-                            <span class="star-badge pull-left">
-                                {forks}
-                                <span class="octicon octicon-repo-forked"></span>
-                            </span>
-                        </div>
                     </div>
                 </li>
-                    '''.format(name=name, description=descr, classified=classified, classified_proba=classified_proba, language=language, stars=stars, forks=forks))
+                    '''.format(name=name, description=descr,
+                    pred_descr=predicted_descr, pred_descr_proba=predicted_descr_proba,
+                    pred_readme=predicted_readme, pred_readme_proba=predicted_readme_proba,
+                    language=language, stars=stars, forks=forks))
     return add_repo_header(repos_html)
 
 
 def build_repo_html(token):
     repo_html = 'No valid GitHub API token received'
     if token != '':
-        model = load_pickle(JOBLIB_DESCRIPTION_PIPELINE_NAME)
+        descr_model = load_pickle(JOBLIB_DESCRIPTION_PIPELINE_NAME)
+        readme_model = load_pickle(JOBLIB_README_PIPELINE_NAME)
         repo_infos = []
         print "Token: " + token
         client = Github(token)
         user = client.get_user()
+        readme_regex = re.compile('[^a-zA-Z0-9 :\/-]')
         for repo in user.get_repos():
-            info = (repo.full_name, repo.description or '', repo.language,
-                    repo.stargazers_count, repo.forks_count)
+            readme = ''
+            try:
+                readme = repo.get_readme().decoded_content
+            except:
+                pass
+            print readme[:10]
+            info = (repo.full_name, repo.description or '',
+                    readme_regex.sub('', readme),
+                    repo.language, repo.stargazers_count, repo.forks_count)
             repo_infos.append(info)
-        return repo_list_html(repo_infos, model)
+        return repo_list_html(repo_infos, descr_model, readme_model)
     return repo_html
