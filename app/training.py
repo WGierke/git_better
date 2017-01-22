0930b1de13d8d10386e7ef58a8c86c431e6a6f9b
Bjarne Sievers <bjrne@users.noreply.github.com>
2017-01-22 23:59:27 +0100

    Latex (#61)
    * Add process graphic
    
    * Remove Chinese and notebook number
    
    * Remove more Chinese
    
    * Move in Appendix

--- app/training.py
+++ app/training.py
@@ -20,11 +20,11 @@ JOBLIB_README_PIPELINE_NAME = 'best_readme_pipeline_5161'
 JOBLIB_VOTING_PIPELINE_NAME = 'best_voting_6774_4667'
 
 
-def find_best_text_pipeline(df_values, labels, pipeline=None, params=None):
+def find_best_text_pipeline(df_values, labels, pipeline=None, parameters=None):
     if not pipeline:
         pipeline = get_text_pipeline()
 
-    if not params:
+    if not parameters:
         parameters = {
             'vect__max_df': (0.5, 0.75, 1.0),
             'vect__ngram_range': ((1, 1), (1, 2), (1, 3)),
